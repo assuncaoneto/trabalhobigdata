289d3d332c201bc496c6816968111b0c697e5f5d
assuncaoneto <150040905+assuncaoneto@users.noreply.github.com>
2023-11-22 14:55:03 -0400

    Update index.py3

--- index.py
+++ index.py
@@ -4,9 +4,7 @@ import datetime
 import history as hist
 import bollinger_bands as bollinger
 import styles
-import streamlit_toggle as tog
 import time
-import pandas as pd  # Certifique-se de importar o pandas
 
 tickers = inv.get_stocks_list("brazil")
 
@@ -24,7 +22,7 @@ with st.sidebar:
     )
 
     date_reference = st.date_input(
-        "Select an initial period",
+        "Select an init of period",
         datetime.datetime.today()
     )
 
@@ -43,41 +41,23 @@ with st.sidebar:
     with toogle_column1:
         st.write(f"Auto Refresh ({sleep_time}s)")
     with toogle_column2:
-        toogle = tog.st_toggle_switch(
-            key="Key1",
-            default_value=False,
-            label_after=False,
-            inactive_color='rgba(255, 75, 75, .5)',
-            active_color="rgb(255, 75, 75)",
-            track_color="rgba(255, 75, 75, .5)"
-        )
-
-# Defina as variáveis antes de usar
-current_value = st.empty()
-min_value = st.empty()
-max_value = st.empty()
-graph = st.empty()
+        toogle = st.checkbox("Toggle Switch")
 
 def prepare_history_visualization():
     history, instance = hist.get(ticker, init_date=init_date, end_date=end_date) if init_date else hist.get(ticker)
+    print("CURRENT PRICE ->", history["Close"].iat[-1])
+    bollinger_figure = bollinger.get(ticker, history)
 
-    max_index = history.index.max()
-    if pd.notna(max_index):
-        max_value = history['Close'][max_index]
+    current_value.metric("Current Value", f"R$ {round(history['Close'][history.index.max()],2)}", f"{round((history['Close'][history.index.max()] / history['Close'][history.index[-2]] - 1) * 100, 2)}%")
+    min_value.metric("Minimum Value", f"R$ {round(history['Close'].min(),2)}", f"{round((history['Close'].min() / history['Close'][history.index.max()] - 1) * 100,2)}%")
+    max_value.metric("Maximum Value", f"R$ {round(history['Close'].max(),2)}", f"{round((history['Close'].max() / history['Close'][history.index.max()] - 1) * 100,2)}%")
 
-        # Use st.write ou st.metric para exibir informações na interface gráfica
-        current_value.metric("Current Value", f"R$ {round(max_value, 2)}", f"{round((max_value / history['Close'][history.index[-2]] - 1) * 100, 2)}%")
-        min_value.metric("Minimum Value", f"R$ {round(history['Close'].min(), 2)}", f"{round((history['Close'].min() / max_value - 1) * 100, 2)}")
-        max_value.metric("Maximum Value", f"R$ {round(max_value, 2)}", f"{round((max_value / history['Close'][max_index] - 1) * 100, 2)}")
-
-        bollinger_figure = bollinger.get(ticker, history)
-        graph.plotly_chart(bollinger_figure, use_container_width=True, sharing="streamlit")
-    else:
-        st.write("No data available for visualization.")
+    graph.plotly_chart(bollinger_figure, use_container_width=True, sharing="streamlit")
 
 if ticker and sleep_time:
-    col1, col2, col3 = st.columns(3)
 
+    col1, col2, col3 = st.columns(3)
+    
     with col1:
         current_value = st.empty()
     with col2:
